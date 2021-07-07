66920f030a54a6bf4304e1de853c3f69bcf665e2
insideplant <kami.10pearl@gmail.com>
2021-07-07 12:34:35 +0000

    fix deploy

--- db/migrate/20210607162929_remove_columns_in_live_organizations.rb
+++ db/migrate/20210607162929_remove_columns_in_live_organizations.rb
@@ -4,7 +4,6 @@ class RemoveColumnsInLiveOrganizations < ActiveRecord::Migration[5.2]
     remove_column :live_organizations, :lecture_at, :date
     remove_column :live_organizations, :integer, :integer
     remove_column :live_organizations, :amount, :integer
-    remove_column :live_organizations, :introduction, :text
     remove_column :live_organizations, :organize_introduction, :text
     add_column :live_organizations, :host, :boolean, default: false, null: false
   end
