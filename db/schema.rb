51ab3b18f7a2973c4ad2f981045b9f6ec25ad2f6
insideplant <kami.10pearl@gmail.com>
2021-08-14 06:18:10 +0000

    add_admin_status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_113712) do
+ActiveRecord::Schema.define(version: 2021_08_14_052152) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -173,6 +173,7 @@ ActiveRecord::Schema.define(version: 2021_07_03_113712) do
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(user_name: "Example User",
+                    email: "example@railstutorial.org",
+                    password: "example",
+                    password_confirmation: "example",
+                    admin: true)
+Band.create!(band_name: "Example",
+             user_id: user.id
+             )
