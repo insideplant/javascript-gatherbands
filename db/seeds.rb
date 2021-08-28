11b1cb657ba8f86210f98b2b7142ecb75929787c
insideplant <kami.10pearl@gmail.com>
2021-08-28 03:13:52 +0000

    make seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create!(user_name: "Example User",
-                    email: "example@railstutorial.org",
-                    password: "example",
-                    password_confirmation: "example",
-                    admin: true)
-Band.create!(band_name: "Example",
-             user_id: user.id
+  user = User.create!(user_name: "Admin User",
+                      email: "example@example.com",
+                      password: "example",
+                      password_confirmation: "example",
+                      admin: true)
+  Band.create!(band_name: "Admin",
+               user_id: user.id
+               )
+
+
+  3.times do |n|
+    a = User.create!(
+      user_name: "Example User#{n + 1}",
+      email: "example#{n + 1}@example.com",
+      profile_image: File.open("./app/assets/images/user#{n + 1}.jpg"),
+      password: "example",
+      password_confirmation: "example",
+    )
+
+    Band.create!(band_name: "Example_band#{n + 1}",
+             user_id: a.id
              )
+  end
+
+  Band.all.each do |band|
+    band.articles.create!(
+      content: 'Hello!',
+    )
+  end
