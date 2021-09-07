14e1d29b6387e0fb58038bc3dbbe4ea527571c5e
insideplant <kami.10pearl@gmail.com>
2021-09-07 15:32:10 +0000

    Hambarger-menu second

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,21 +72,5 @@ window.onload = function(){
       }
     })
   });
-  
-  const open = document.getElementById('open');
-  const close = document.getElementById('close');
-  const modal = document.getElementById('modal');
-  const mask = document.getElementById('mask');
-  
-  open.addEventListener('click',()=>{
-    modal.classList.remove('hidden');
-    mask.classList.remove('hidden')
-  });
-  
-  close.addEventListener('click',()=>{
-    modal.classList.add('hidden');
-    mask.classList.add('hidden');
-  });
-  
 };
 
--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -2,12 +2,12 @@
 
 
 {
-   const open = document.getElementById('open');
+  const modalopen = document.getElementById('modalopen');
   const shut = document.getElementById('shut');
   const modal = document.getElementById('modal');
   const mask = document.getElementById('mask');
   
-  open.addEventListener('click',()=>{
+  modalopen.addEventListener('click',()=>{
     modal.classList.remove('hidden');
     mask.classList.remove('hidden')
   });
